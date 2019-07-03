0aef11bdc512c0916ac525c60f27ab8af06d7084
dada99 <thedada99@gmail.com>
2019-07-03 22:36:37 +0800

    try add_ur_rule fucntion

--- flask_4.py
+++ flask_4.py
@@ -1,6 +1,15 @@
-from flask import Flask # This step will import some variables, such as __name__
+from flask import Flask# This step will import some variables, such as __name__
 #app = Flask("db") # Because db is a real package, so the root path ,static path variables of app object will change to "db" 's directory
 #app = Flask("db1") # Because db1 does not exist, so app's variable wouldn't change
 #app = Flask("fake_module") # Same as db1 even though fake_module is a real python module, but just a single file
 app = Flask(__name__)
-print(app.name)
+#print(app.name)
+
+def index():
+    return 'This is the index'
+app.add_url_rule("/",'',index)
+#app.add_url_rule("/",view_func=index) # Same Ok
+
+@app.route("/index")
+def index1():
+    return 'This is the index'
