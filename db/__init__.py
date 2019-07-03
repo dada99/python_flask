2c494e999f6e5252b70484fd831f638ee5d55bec
dada99 <thedada99@gmail.com>
2019-07-03 21:43:26 +0800

    change the requirements file for security issue

---
+++ db/__init__.py
@@ -0,0 +1,2 @@
+def db_func1():
+    pass
--- flask_4.py
+++ flask_4.py
@@ -1,3 +1,6 @@
-from flask import Flask
-#app = Flask("myapplications")
-print(__name__)
+from flask import Flask # This step will import some variables, such as __name__
+#app = Flask("db") # Because db is a real package, so the root path ,static path variables of app object will change to "db" 's directory
+#app = Flask("db1") # Because db1 does not exist, so app's variable wouldn't change
+#app = Flask("fake_module") # Same as db1 even though fake_module is a real python module, but just a single file
+app = Flask(__name__)
+print(app.name)
