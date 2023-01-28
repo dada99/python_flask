09db07abb57f28afab744480b27b1b438d60cdb1
dada99 <dada99@163.com>
2023-01-28 20:35:44 +0800

    modify comment of route /projects/

--- flask-1.py
+++ flask-1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route('/projects/') # url with / can be map by /projects/ or /project. / will be added automaticaly
+@app.route('/projects/') # url with / can be map by /projects/ or /projects. / will be added automaticaly
 def projects():
     return 'The project page'
 
